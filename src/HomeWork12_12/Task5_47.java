47c1011dcac08dd0a1ea9501d25efb0e59691510
admin <demar.demidov@yandex.ru>
2023-12-14 01:54:57 +0300

    test

--- src/HomeWork12_12/Task5_47.java
+++ src/HomeWork12_12/Task5_47.java
@@ -1,5 +1,7 @@
 package HomeWork12_12;
+
 import java.util.Scanner;
+
 /*
 Даны числа a1, a2, a3, a4, a5, a6.
 Определить их произведение.
@@ -7,19 +9,13 @@ import java.util.Scanner;
 public class Task5_47 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите a1: ");
-        int a1 = scanner.nextInt();
-        System.out.print("Введите a2: ");
-        int a2 = scanner.nextInt();
-        System.out.print("Введите a3: ");
-        int a3 = scanner.nextInt();
-        System.out.print("Введите a4: ");
-        int a4 = scanner.nextInt();
-        System.out.print("Введите a5: ");
-        int a5 = scanner.nextInt();
-        System.out.print("Введите a6: ");
-        int a6 = scanner.nextInt();
-        int multi = a1*a2*a3*a4*a5*a6;
-        System.out.println("Произведение всех чисел равно: " + multi);
+        System.out.println("Введите 6 чисел: ");
+        int multi = 1;
+        for (int i = 1; i <7 ; i++) {
+            int a = scanner.nextInt();
+            multi *=a;
+        }
+        System.out.print("Произведение всех чисел равно: " + multi);
+
     }
 }
--- src/HomeWork12_12/Task5_57.java
+++ src/HomeWork12_12/Task5_57.java
@@ -7,26 +7,15 @@ import java.util.Scanner;
 public class Task5_57 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите оценку Васи по математике: ");
-        int a1 = scanner.nextInt();
-        System.out.print("Введите оценку Васи по русскому: ");
-        int a2 = scanner.nextInt();
-        System.out.print("Введите оценку Васи по химии: ");
-        int a3 = scanner.nextInt();
-        System.out.print("Введите оценку Васи по биологии: ");
-        int a4 = scanner.nextInt();
-        int sum = a1+a2+a3+a4;
-        System.out.println("Сумма оценок Васи: " + sum);
+        for (int i = 1; i <=2 ; i++) {
+            int sumOfSubjects = 0;
+            for (int j = 1; j <= 4; j++) {
+                System.out.println("Введите оценку по " + j + " предмету " + i + " ученика");
+                int mark = scanner.nextInt();
+                sumOfSubjects +=mark;
+            }
+            System.out.printf("Сумма оценок %s ученика равна %s\n", i, sumOfSubjects);
 
-        System.out.print("Введите оценку Коли по математике: ");
-        int b1 = scanner.nextInt();
-        System.out.print("Введите оценку Коли по русскому: ");
-        int b2 = scanner.nextInt();
-        System.out.print("Введите оценку Коли по химии: ");
-        int b3 = scanner.nextInt();
-        System.out.print("Введите оценку Коли по биологии: ");
-        int b4 = scanner.nextInt();
-        int sum2 = b1+b2+b3+b4;
-        System.out.println("Сумма оценок Коли: " + sum2);
+        }
     }
 }
