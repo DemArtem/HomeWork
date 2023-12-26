b5aff8845638bfbbf325722835814456540c3281
Artem Demidov <demar.demidov@yandex.ru>
2023-12-26 13:15:23 +0300

    test

---
+++ src/HomeWork12_26/Task5_86.java
@@ -0,0 +1,10 @@
+package HomeWork12_26;
+/*
+Составить программу возведения натурального числа в квадрат, учитывая
+следующую закономерность: n n 1 3 5 7 9 ... 2 1 .
+ */
+public class Task5_86 {
+    public static void main(String[] args) {
+
+    }
+}
