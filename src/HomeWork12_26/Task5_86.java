e87d97515582fb5dc93d6f68319f75a15e0a92e8
Artem Demidov <demar.demidov@yandex.ru>
2023-12-28 13:43:46 +0300

    test

--- src/HomeWork12_26/Task5_86.java
+++ src/HomeWork12_26/Task5_86.java
@@ -1,10 +1,23 @@
+
 package HomeWork12_26;
+
+import java.util.Scanner;
+
 /*
 Составить программу возведения натурального числа в квадрат, учитывая
-следующую закономерность: n n 1 3 5 7 9 ... 2 1 .
+следующую закономерность:
  */
 public class Task5_86 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Введите число: ");
+        int num = scanner.nextInt();
+        int res = 0;
+
+        for (int i = 1; i<=num; i++) {
+            res+=num;
+        }
+        System.out.println("Квадрат числа " + num + " равен " +res);
 
     }
 }
