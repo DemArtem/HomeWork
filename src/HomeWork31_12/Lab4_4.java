e19a85b3bece43eaf4317574753440227fee01cb
admin <demar.demidov@yandex.ru>
2024-01-06 13:25:52 +0300

    test

---
+++ src/HomeWork31_12/Lab4_4.java
@@ -0,0 +1,14 @@
+package HomeWork31_12;
+
+public class Lab4_4 {
+    public static void main(String[] args) {
+        int[] arr = {4,2,3,5,8,7,6,4,8};
+        for (int i: arr) {
+            for (int j: arr){
+
+            }
+
+            System.out.println(i);
+        }
+    }
+}
