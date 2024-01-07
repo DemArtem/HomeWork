596c97c91979e88decb95f803aafc4e1e534b30f
admin <demar.demidov@yandex.ru>
2024-01-07 21:13:49 +0300

    test

--- src/HomeWork31_12/Examples.java
+++ src/HomeWork31_12/Examples.java
@@ -3,6 +3,7 @@ package HomeWork31_12;
 public class Examples {
     public static void main(String[] args) {
         int[] arr = {1, 22, 3, 4, 5, 62};
+
         //вывод на экран всего массива
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
